e4989464f35aaff066f4124324465a73b62a0484
Claas <claas@claas.dev>
2024-06-12 17:43:43 +0200

    Add temporary home page

--- eleventy/tailwind.config.js
+++ eleventy/tailwind.config.js
@@ -1,7 +1,7 @@
 import materialTailwind from "@claas.dev/material-tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.html", "./main.js", "./cv.njk", "!./_site/**/*"],
+  content: ["./**/*.html", "./main.js", "./*.njk", "!./_site/**/*"],
 
   theme: {
     extend: {},
