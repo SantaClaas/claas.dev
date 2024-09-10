a97e3daea02630d8a2f93a7e6b51fdef45e0e227
Claas <claas@claas.dev>
2024-09-10 22:20:09 +0200

    Add design page to get a look at colors

--- eleventy/tailwind.config.js
+++ eleventy/tailwind.config.js
@@ -2,7 +2,13 @@ import materialTailwind from "@claas.dev/material-tailwind";
 import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,webc,njk}", "./main.js", "!./_site/**/*"],
+  darkMode: "selector",
+  content: [
+    "./**/*.{html,webc,njk}",
+    "./main.js",
+    "!./_site/**/*",
+    "./design.md",
+  ],
 
   theme: {
     extend: {},
