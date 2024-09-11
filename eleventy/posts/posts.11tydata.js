25cebed84e12520c7086bf3810ac0725ed6b797c
Claas <claas@claas.dev>
2024-09-11 15:44:50 +0200

    Add headline view transition

--- eleventy/posts/posts.11tydata.js
+++ eleventy/posts/posts.11tydata.js
@@ -1,3 +1,12 @@
-const data = { layout: "post.webc", tags: ["post"], date: "git Created" };
+let counter = 0;
+const data = {
+  layout: "post.webc",
+  tags: ["post"],
+  date: "git Created",
+  eleventyComputed: {
+    // As simple unique id to use for view transitions. It is ok that this might leak count
+    uniqueId: (_data) => counter++,
+  },
+};
 // if (process.env.NODE_ENV === "production") data.date = "git Last Modified";
 export default data;
