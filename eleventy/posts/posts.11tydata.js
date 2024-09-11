463ba1270c7e988f0cbbca5c860823909dc7c2ce
Claas <claas@claas.dev>
2024-09-11 17:10:40 +0200

    Fix common pitfall

--- eleventy/posts/posts.11tydata.js
+++ eleventy/posts/posts.11tydata.js
@@ -6,7 +6,7 @@ export default {
   layout: "post.webc",
   tags: ["post"],
   date: "git Created",
-  permalink: ({ title }) => `/posts/${slugify(title, { lower: true })}`,
+  permalink: ({ title }) => `/posts/${slugify(title, { lower: true })}/`,
   eleventyComputed: {
     // As simple unique id to use for view transitions. It is ok that this might leak count
     uniqueId: (_data) => counter++,
